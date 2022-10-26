9952ef0801b65401bca6659d64ca501fc9bc46c4
DCrawley94 <duncan.crawley@gmail.com>
2022-10-26 10:59:02 +0100

    update kata ranking

--- create-kata.sh
+++ create-kata.sh
@@ -5,4 +5,4 @@ KATA_NAME=$1
 mkdir "$KATA_NAME" && touch "$KATA_NAME/$KATA_NAME.md" "$KATA_NAME/$KATA_NAME.py"
 
 echo -e "def $KATA_NAME():\n\tpass" >"$KATA_NAME/$KATA_NAME.py"
-echo -e "# $KATA_NAME" >"$KATA_NAME/$KATA_NAME.md"
+echo -e "# $KATA_NAME\n\n\n## Examples" >"$KATA_NAME/$KATA_NAME.md"
