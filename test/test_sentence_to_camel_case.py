807eb7ea285ace74f78d2410cff705d0cd99f723
Verity Gregory <verity.gregory@gmail.com>
2022-11-11 13:47:21 +0000

    fix incorrect test description

--- test/test_sentence_to_camel_case.py
+++ test/test_sentence_to_camel_case.py
@@ -1,7 +1,7 @@
 from sentence_to_camel_case.sentence_to_camel_case import sentence_to_camel_case
 
 
-def test_returns_a_single_word_in_lower_if_true():
+def test_returns_a_single_word_in_upper_if_true():
     expected = 'This'
     result = sentence_to_camel_case('this', True)
     print(f'Result is: {result}')
