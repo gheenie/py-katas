2917b13d27702262ed29e3095acaa8bc33af7b3e
infoomics <sbx_94@hotmail.com>
2023-02-22 13:00:41 +0000

    initial code and tests

--- src/sentence_to_camel_case/sentence_to_camel_case.py
+++ src/sentence_to_camel_case/sentence_to_camel_case.py
@@ -1,2 +1,14 @@
-def sentence_to_camel_case(arg1, arg2):
-    pass
+def sentence_to_camel_case(sentence, should_capitalise):
+    if sentence == '':
+        return ''
+    
+    words = sentence.split(' ')
+
+    camel_cased_words = list( map(lambda word: word.lower().capitalize(), words) )
+
+    camel_cased_sentence = ''.join(camel_cased_words)
+
+    if not should_capitalise:
+        camel_cased_sentence = camel_cased_sentence[0].lower() + camel_cased_sentence[1:]
+    
+    return camel_cased_sentence
--- test/test_sentence_to_camel_case.py
+++ test/test_sentence_to_camel_case.py
@@ -7,3 +7,32 @@ def test_returns_a_single_word_in_upper_if_true():
     result = sentence_to_camel_case('this', True)
     print(f'Result is: {result}')
     assert result == expected
+
+def test_returns_a_single_word_in_lower_if_false():
+    expected = 'this'
+    result = sentence_to_camel_case('this', False)
+    print(f'Result is: {result}')
+    assert result == expected
+
+def test_all_spaced_words_capitalised_if_true():
+    expected = 'ThisSentence'
+    result = sentence_to_camel_case('this sentence', True)
+    print(f'Result is: {result}')
+    assert result == expected
+
+def test_all_except_1st_spaced_words_capitalised_if_false():
+    expected = 'thisSentence'
+    result = sentence_to_camel_case('this sentence', False)
+    print(f'Result is: {result}')
+    assert result == expected
+
+    expected = 'thisBiggerStrangeSentence'
+    result = sentence_to_camel_case('This Bigger strange Sentence', False)
+    print(f'Result is: {result}')
+    assert result == expected
+
+def test_empty_string():
+    expected = ''
+    result = sentence_to_camel_case('', False)
+    print(f'Result is: {result}')
+    assert result == expected
