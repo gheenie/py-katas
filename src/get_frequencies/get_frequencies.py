5098ecc941c263c5a1cdc3e668dba915c86ec35f
infoomics <sbx_94@hotmail.com>
2023-02-22 15:12:58 +0000

    initial code and tests

--- src/get_frequencies/get_frequencies.py
+++ src/get_frequencies/get_frequencies.py
@@ -1,2 +1,10 @@
-def get_frequencies():
-	pass
+def get_frequencies(string):
+	frequency_table = {}
+
+	for char in string:
+		if not char.isalpha():
+			continue
+
+		frequency_table[char] = frequency_table.get(char, 0) + 1
+	
+	return frequency_table
---
+++ test/test_get_frequencies.py
@@ -0,0 +1,26 @@
+from src.get_frequencies.get_frequencies import (
+    get_frequencies)
+
+
+def test_one_word():
+    expected = {
+        'h': 1,
+        'e': 1,
+        'l': 2,
+        'o': 1
+    }
+
+    assert get_frequencies('hello') == expected
+
+def test_multiple_words_ignore_spaces():
+    expected = {
+        'h': 1,
+        'e': 1,
+        'l': 3,
+        'o': 2,
+        'w': 1,
+        'r': 1,
+        'd': 1
+    }
+
+    assert get_frequencies('hello world') == expected
