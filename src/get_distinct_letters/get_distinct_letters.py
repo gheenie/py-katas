966290667af14313c722107455f27422bd6571a0
infoomics <sbx_94@hotmail.com>
2023-02-22 13:48:20 +0000

    initial code and tests

--- src/get_distinct_letters/get_distinct_letters.py
+++ src/get_distinct_letters/get_distinct_letters.py
@@ -1,2 +1,10 @@
-def get_distinct_letters():
-	pass
+def get_distinct_letters(string1, string2):
+	set1 = set(string1)
+	set2 = set(string2)
+
+	unique_letters = set1.symmetric_difference(set2)
+	
+	unique_letters_alphabetical = list(unique_letters)
+	unique_letters_alphabetical.sort()
+	
+	return ''.join(unique_letters_alphabetical)
---
+++ test/test_get_distinct_letters.py
@@ -0,0 +1,21 @@
+from src.get_distinct_letters.get_distinct_letters import (
+    get_distinct_letters)
+
+
+def test_no_distinct_letters():
+    assert get_distinct_letters('eaf', 'fae') == ''
+
+def test_distinct_letters_from_second_word():
+    assert get_distinct_letters('eaf', 'faeop') == 'op'
+
+def test_distinct_letters_from_first_word():
+    assert get_distinct_letters('opeaf', 'fae') == 'op'
+
+def test_distinct_but_multiple_letters_from_first_word():
+    assert get_distinct_letters('opeafop', 'fae') == 'op'
+
+def test_distinct_letters_from_both_words__multiple_from_first():
+    assert get_distinct_letters('opeafop', 'qfaew') == 'opqw'
+
+def test_specific_examples():
+    assert get_distinct_letters('hello', 'worldoo') == 'dehrw'
