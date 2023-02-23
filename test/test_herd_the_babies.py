f98ce494cb0ad284b31250bea702568bb8964d1b
infoomics <sbx_94@hotmail.com>
2023-02-23 12:53:50 +0000

    first complete tests

---
+++ test/test_herd_the_babies.py
@@ -0,0 +1,11 @@
+from src.herd_the_babies.herd_the_babies import (
+    herd_the_babies)
+
+def test_1_family():
+    assert herd_the_babies('aA') == 'Aa'
+
+def test_multiple_families_some_without_babies():
+    assert herd_the_babies('aBA') == 'AaB'
+
+def test_multiple_families():
+    assert herd_the_babies('bbaBccAC') == 'AaBbbCcc'
