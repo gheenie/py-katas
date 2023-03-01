1d7f07586fefffbd4432ab5daafb9b02229efc66
infoomics <sbx_94@hotmail.com>
2023-03-01 16:14:52 +0000

    add initial code to complete the task

--- src/herd_the_babies/herd_the_babies.py
+++ src/herd_the_babies/herd_the_babies.py
@@ -1,3 +1,17 @@
+from itertools import groupby
 
 def herd_the_babies(string):
-    pass
+    letters = list(string)
+
+    # Sorts aA-zZ
+    letters.sort(key=str.lower)
+
+    # Will be a list of list. Each innermost list contains both upper and lower of one alphabet.
+    alphabet_groups = []
+
+    for k, g in groupby(letters, key=lambda x: x.lower()):
+        # g will be a group of one alphabet, eg aaAA
+        alphabet_groups.append(sorted(list(g)))
+
+    # Flatten the list, then join into a str
+    return ''.join([letter for alphabet_group in alphabet_groups for letter in alphabet_group])
