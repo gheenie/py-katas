965d76d9b1050d50d0aae2204d2845e66522d68c
infoomics <sbx_94@hotmail.com>
2023-03-01 17:42:03 +0000

    add initial code to pass happy paths

--- src/sum_ascii/sum_ascii.py
+++ src/sum_ascii/sum_ascii.py
@@ -1,3 +1,16 @@
-
 def sum_ascii(array):
-    pass
+    highest_ascii_sum = 0
+    name_with_highest_ascii_sum = ''
+
+    for name in array:
+        ascii_sum = 0
+
+        for letter in name: ascii_sum += ord(letter.lower())
+
+        if ascii_sum > highest_ascii_sum:
+            highest_ascii_sum = ascii_sum
+            name_with_highest_ascii_sum = name
+
+        print(ascii_sum)
+
+    return name_with_highest_ascii_sum
