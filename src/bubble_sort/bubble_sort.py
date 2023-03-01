5d45873338fa7e61222abdd8c12cc3e1f618d18c
infoomics <sbx_94@hotmail.com>
2023-03-01 18:40:46 +0000

    add initial code to complete the task

--- src/bubble_sort/bubble_sort.py
+++ src/bubble_sort/bubble_sort.py
@@ -1,2 +1,25 @@
-def bubble_sort():
-	pass
+import copy
+
+def bubble_sort(array, calls=0):
+	if len(array) == 0: return []
+
+	nums = copy.deepcopy(array)
+
+	if len(array) == 1: return nums
+
+	length_nums_minus_1 = len(nums) - 1
+
+	for i in range(length_nums_minus_1):
+		current_num = nums[i]
+		next_num = nums[i + 1]
+
+		if current_num > next_num:
+			nums[i] = next_num
+			nums[i + 1] = current_num
+
+	calls += 1
+
+	if calls == length_nums_minus_1:
+		return nums
+
+	return bubble_sort(nums, calls)
