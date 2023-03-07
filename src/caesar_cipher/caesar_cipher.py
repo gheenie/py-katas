27ce78b1dc5c87df7651c08442fed268f8d18839
infoomics <sbx_94@hotmail.com>
2023-03-07 20:04:42 +0000

    add first code to complete task

--- src/caesar_cipher/caesar_cipher.py
+++ src/caesar_cipher/caesar_cipher.py
@@ -1,3 +1,18 @@
+import re
 
-def caesar(string, number):
-    pass
+
+def caesar_cipher(string, number):
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    ciphered_string = ''
+    # In python, % is modulo not remainder.
+    # So it will still work nicely for positive number.
+    shift_by = number % -26
+    
+    for char in string:
+        if re.match(r'[A-Za-z]', char) == None:
+            ciphered_string += char
+        else:
+            alphabet_index = alphabets.index(char)
+            ciphered_string += alphabets[alphabet_index + shift_by]
+    
+    return ciphered_string
