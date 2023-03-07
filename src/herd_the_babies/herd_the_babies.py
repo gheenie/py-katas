db5c7afb8b2e79436e905a83f41e6bcb7543fee7
infoomics <sbx_94@hotmail.com>
2023-03-07 17:12:41 +0000

    use more efficient sort key

--- src/herd_the_babies/herd_the_babies.py
+++ src/herd_the_babies/herd_the_babies.py
@@ -3,15 +3,6 @@ from itertools import groupby
 def herd_the_babies(string):
     letters = list(string)
 
-    # Sorts aA-zZ
-    letters.sort(key=str.lower)
+    letters.sort(key=lambda char: (char.upper(), char))
 
-    # Will be a list of list. Each innermost list contains both upper and lower of one alphabet.
-    alphabet_groups = []
-
-    for k, g in groupby(letters, key=lambda x: x.lower()):
-        # g will be a group of one alphabet, eg aaAA
-        alphabet_groups.append(sorted(list(g)))
-
-    # Flatten the list, then join into a str
-    return ''.join([letter for alphabet_group in alphabet_groups for letter in alphabet_group])
+    return ''.join(letters)
