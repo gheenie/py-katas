8600f116aa173403d03857727f640d8b4d2fe1bf
infoomics <sbx_94@hotmail.com>
2023-03-15 20:15:21 +0000

    add first method bodies to tests

--- test/test_roman_numeral_encoder.py
+++ test/test_roman_numeral_encoder.py
@@ -10,12 +10,25 @@ def test_1_to_9():
 
 
 def test_10_to_39():
-    pass
+    assert roman_numeral_encoder(11) == 'XI'
+    assert roman_numeral_encoder(24) == 'XXIV'
+    assert roman_numeral_encoder(28) == 'XXVIII'
+    assert roman_numeral_encoder(39) == 'XXXIX'
 
 
 def test_40_to_69():
-    pass
+    assert roman_numeral_encoder(40) == 'XL'
+    assert roman_numeral_encoder(42) == 'XLII'
+    assert roman_numeral_encoder(49) == 'XLIX'
+    assert roman_numeral_encoder(50) == 'L'
+    assert roman_numeral_encoder(55) == 'LV'
+    assert roman_numeral_encoder(59) == 'LIX'
+    assert roman_numeral_encoder(60) == 'LX'
+    assert roman_numeral_encoder(67) == 'LXVII'
+    assert roman_numeral_encoder(69) == 'LXIX'
 
 
 def test_90_to_119():
-    pass
+    assert roman_numeral_encoder(92) == 'XCII'
+    assert roman_numeral_encoder(105) == 'CV'
+    assert roman_numeral_encoder(116) == 'CXVI'
