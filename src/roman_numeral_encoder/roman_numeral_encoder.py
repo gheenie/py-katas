ca4743f0348633873d0abf0c9623dece0a6c5d55
infoomics <sbx_94@hotmail.com>
2023-04-29 21:38:18 +0100

    add passing code for test 1 to 9

--- src/roman_numeral_encoder/roman_numeral_encoder.py
+++ src/roman_numeral_encoder/roman_numeral_encoder.py
@@ -1,8 +1,24 @@
-def roman_numeral_encoder():
-	# evaluate the -1 digit
+def roman_numeral_encoder(num):
+	last_digit_translation = {
+		1: 'I',
+		2: 'II',
+		3: 'III',
+		4: 'IV',
+		5: 'V',
+		6: 'VI',
+		7: 'VII',
+		8: 'VIII',
+		9: 'IX',
+	}
+
+	roman_equivalent = ''
+
+	# Evaluate the -1 digit
+	last_digit = num % 10
+	roman_equivalent += last_digit_translation[last_digit]
 
 	# evaluate the -2 digit
 
 	# evaluate -3 digit onwards 
 	
-	pass
+	return roman_equivalent
