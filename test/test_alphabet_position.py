bf5088ad7e86c558d49b3638165428c2e161989d
infoomics <sbx_94@hotmail.com>
2023-04-30 17:34:15 +0100

    add first 3 tests

---
+++ test/test_alphabet_position.py
@@ -0,0 +1,13 @@
+from src.alphabet_position.alphabet_position import alphabet_position
+
+
+def test_only_alphabets():
+    assert alphabet_position('abc') == '1 2 3'
+
+
+def test_alphabets_and_non_alphabets():
+    assert alphabet_position("The sunset sets at twelve o' clock.") == '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+
+
+def test_only_non_alphabets():
+    assert('1234567890!@#$%^&*(),.') == ''
