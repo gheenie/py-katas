4f914b44567a607ee4e84eabfd81089f4df730d5
infoomics <sbx_94@hotmail.com>
2023-06-06 17:02:21 +0100

    add code for first test cases

--- src/count_bits/count_bits.py
+++ src/count_bits/count_bits.py
@@ -1,2 +1,12 @@
 def count_bits(integer):
-  pass
+  count_1 = 0
+
+  while integer != 0:
+    if integer % 2 == 1:
+      integer -= 1
+      count_1 += 1
+      integer /= 2
+    else:
+      integer /= 2
+  
+  return count_1
